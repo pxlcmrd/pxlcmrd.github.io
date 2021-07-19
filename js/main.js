7549209bef1cf87b2bdc6198ab2ce3ba01920bf4
Raphael Silveira <raphaelsilveira@cptsoftware.com.br>
2021-07-19 17:21:00 -0300

    Add lista top gêneros e formatos da coleção

--- js/main.js
+++ js/main.js
@@ -73,3 +73,102 @@ var conteudo_artistas = alasql("SELECT *, count(*) AS num FROM ? WHERE artists_s
 //console.log(conteudo_artistas);
 
 document.getElementById("top_artistas").innerHTML = conteudo_artistas;
+
+var generos = {};
+var formatos = {};
+
+list.forEach(function(e) {
+    (e.styles || e.genres).forEach(function(g) {
+        if (!generos[g]) {
+            generos[g] = 1;
+        } else {
+            generos[g]++;
+        }
+    });
+    e.formats.forEach(function(f) {
+        var nome = f.name;
+
+        if (f.descriptions && nome == "Vinyl") {
+            nome += ' ' + (f.descriptions.indexOf("LP") >= 0 ? '12"' : f.descriptions[0]);
+        }
+
+        if (nome == "Vinyl Album" || nome == "Vinyl Compilation") {
+            debugger;
+        }
+
+        if (!formatos[nome]) {
+            formatos[nome] = 1;
+        } else {
+            formatos[nome]++;
+        }
+    });
+});
+
+var top5_generos = Object.keys(generos).sort(function(a, b) {
+    return generos[a] > generos[b] ? -1 : 1;
+});
+
+var top5_formatos = Object.keys(formatos).sort(function(a, b) {
+    return formatos[a] > formatos[b] ? -1 : 1;
+});
+
+var conteudo_generos = "";
+var conteudo_formatos = "";
+
+for (var i = 0; i < 5; i++) {
+    conteudo_generos +=
+        '<li class="single-item">' +
+        '  <a data-link="" data-title="Got What I Got" data-artist="Jason Aldean" data-img="img/covers/cover.svg" href="#" class="single-item__cover">' +
+        '    <img src="img/covers/cover.svg" alt="">' +
+        '    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">' +
+        '      <path d="M18.54,9,8.88,3.46a3.42,3.42,0,0,0-5.13,3V17.58A3.42,3.42,0,0,0,7.17,21a3.43,3.43,0,0,0,1.71-.46L18.54,15a3.42,3.42,0,0,0,0-5.92Zm-1,4.19L7.88,18.81a1.44,1.44,0,0,1-1.42,0,1.42,1.42,0,0,1-.71-1.23V6.42a1.42,1.42,0,0,1,.71-1.23A1.51,1.51,0,0,1,7.17,5a1.54,1.54,0,0,1,.71.19l9.66,5.58a1.42,1.42,0,0,1,0,2.46Z">' +
+        '      </path>' +
+        '    </svg>' +
+        '    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">' +
+        '      <path d="M16,2a3,3,0,0,0-3,3V19a3,3,0,0,0,6,0V5A3,3,0,0,0,16,2Zm1,17a1,1,0,0,1-2,0V5a1,1,0,0,1,2,0ZM8,2A3,3,0,0,0,5,5V19a3,3,0,0,0,6,0V5A3,3,0,0,0,8,2ZM9,19a1,1,0,0,1-2,0V5A1,1,0,0,1,9,5Z">' +
+        '      </path>' +
+        '    </svg>' +
+        '  </a>' +
+        '  <div class="single-item__title">' +
+        '    <h4>' +
+        '    <a href="#">' + top5_generos[i] +
+        '    </a></h4>' +
+        //'    <span>' +
+        //'      <a href="#">Jason Aldean' +
+        //'      </a>' +
+        //'    </span>' +
+        '  </div>' +
+        '  <span class="single-item__time">' + generos[top5_generos[i]] +
+        '  </span>' +
+        '</li>';
+
+    conteudo_formatos +=
+        '<li class="single-item">' +
+        '<i class="fas fa-2x fa-' + (top5_formatos[i].indexOf('Vinyl') > -1 ? 'record-vinyl' : 'compact-disc') + '"></i>' +
+        '  <a data-link="" data-title="Supalonely" data-artist="BENEE Featuring" data-img="img/covers/cover9.jpg" href="https://dmitryvolkov.me/demo/blast2.0/audio/9106709_epic-adventure-cinematic-trailer_by_audiopizza_preview.mp3" class="single-item__cover">' +
+        /*'    <img src="img/covers/cover9.jpg" alt="">' +
+        '    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">' +
+        '      <path d="M18.54,9,8.88,3.46a3.42,3.42,0,0,0-5.13,3V17.58A3.42,3.42,0,0,0,7.17,21a3.43,3.43,0,0,0,1.71-.46L18.54,15a3.42,3.42,0,0,0,0-5.92Zm-1,4.19L7.88,18.81a1.44,1.44,0,0,1-1.42,0,1.42,1.42,0,0,1-.71-1.23V6.42a1.42,1.42,0,0,1,.71-1.23A1.51,1.51,0,0,1,7.17,5a1.54,1.54,0,0,1,.71.19l9.66,5.58a1.42,1.42,0,0,1,0,2.46Z">' +
+        '      </path>' +
+        '    </svg>' +
+        '    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">' +
+        '      <path d="M16,2a3,3,0,0,0-3,3V19a3,3,0,0,0,6,0V5A3,3,0,0,0,16,2Zm1,17a1,1,0,0,1-2,0V5a1,1,0,0,1,2,0ZM8,2A3,3,0,0,0,5,5V19a3,3,0,0,0,6,0V5A3,3,0,0,0,8,2ZM9,19a1,1,0,0,1-2,0V5A1,1,0,0,1,9,5Z">' +
+        '      </path>' +
+        '    </svg>' +*/
+        '  </a>' +
+        '  <div class="single-item__title">' +
+        '    <h4>' +
+        '    ' + top5_formatos[i] +
+        '    </h4>' +
+        //'    <span>' +
+        //'      <a href="artist.html">BENEE Featuring' +
+        //'      </a>' +
+        //'    </span>' +
+        '  </div>' +
+        '  <span class="single-item__time">' + formatos[top5_formatos[i]] +
+        '  </span>' +
+        '</li>';
+}
+
+document.getElementById("top_generos").innerHTML = conteudo_generos;
+document.getElementById("top_formatos").innerHTML = conteudo_formatos;
