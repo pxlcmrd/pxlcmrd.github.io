a794e18060d979b435c8ca3462530f41886b8890
Raphael Silveira <raphaelsilveira@cptsoftware.com.br>
2022-03-02 17:11:16 -0300

    Script para verificar erros nos releases

--- verifica.py
+++ verifica.py
@@ -1,19 +1,16 @@
 """ Confere quais os releases não têm imagem de capa """
-import csv
-from argparse import ArgumentParser
 import os
+import re
+from json import loads
 import math
-
-def parse_args():
-    """ Trata os argumentos passados na linha de comando """
-    parser = ArgumentParser()
-    parser.add_argument('file', metavar='Arquivo CSV', help='arquivo csv da coleção')
-    return parser.parse_args()
 
 def print_table(lista_sem_img):
     """ Imprime uma tabela com os nomes dos releases sem imagem """
     titulos = ['id', 'Artista', 'Título']
     max_width = [len(titulos[0]), len(titulos[1]), len(titulos[2])]
+
+    if len(lista_sem_img) == 0:
+        return 0;
 
     for release in lista_sem_img:
         max_width[0] = max(max_width[0], len(release[0]))
@@ -25,8 +22,8 @@ def print_table(lista_sem_img):
         ('─' * (max_width[2] + 2)) + '┐')
 
     cabecalho = ''
-    for i in enumerate(titulos):
-        cabecalho = '│ ' + (' ' * math.floor((max_width[i] -
+    for i in range(len(titulos)):
+        cabecalho = cabecalho + '│ ' + (' ' * math.floor((max_width[i] -
         len(titulos[i])) / 2)) + titulos[i] + (' ' * math.ceil(1 + (max_width[i] -
         len(titulos[i])) / 2))
 
@@ -46,41 +43,28 @@ def print_table(lista_sem_img):
 def main():
     """ Função principal """
     try:
-        with open(args.file, encoding="utf8") as csv_file:
+        with open('js/list.js', encoding="utf8") as arquivo:
             #Inicializa os contadores
             count = 0
+            count_erro_track = 0
             line_count = 0
-            col_id = 0
-            col_title = 0
-            col_artist = 0
             lista_sem_img = []
+            lista_com_erro = '';
 
-            #Lê o conteúdo do csv
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            list_discogs = list(csv_reader)
+            #Lê o conteúdo do list.js
+            list_discogs = loads(arquivo.readline()[11:-1])
 
             for row in list_discogs:
-                #A primeira linha do csv é apenas os títulos das colunas
-                if line_count == 0:
-                    idx_col = 0
-                    for coluna in row:
-                        #Busca o índice da coluna 'release_id'
-                        if coluna == 'release_id':
-                            col_id = idx_col
-                        elif coluna == 'Title':
-                            col_title = idx_col
-                        elif coluna == 'Artist':
-                            col_artist = idx_col
-                        idx_col += 1
+                #Tenta ver se a imagem do release lido do list.js existe na pasta
+                if not os.path.exists('img/' + str(row['id']) + '.jpg'):
+                    lista_sem_img.append([str(row['id']), row['artists_sort'], row['title']])
+                    count += 1
 
-                    if col_id == col_title == col_artist:
-                        raise TypeError
-                else:
-                    #Tenta ver se a imagem do release lido do csv já existe na pasta
-                    if not os.path.exists('img/' + row[col_id] + '.jpg'):
-                        #Se existir ele é reaproveitado
-                        lista_sem_img.append([row[col_id], row[col_artist], row[col_title]])
-                        count += 1
+                for track in row['tracklist']:
+                    if track['type_'] == 'track' and not re.match(r"^(Video)? ?[A-Z\d]+[.-]?[A-Za-z\d]* ?$", track['position']):
+                        lista_com_erro = lista_com_erro + ('\n' + str(row['id']) + ' - ' + row['title'] + ' (' + track['position'] + ')')
+                        count_erro_track += 1
+                        break
 
                 line_count += 1
 
@@ -90,11 +74,14 @@ def main():
 
             print_table(lista_sem_img)
 
-    except FileNotFoundError:
-        print("Arquivo não encontrado: " + args.file)
-    except TypeError:
-        print('O arquivo não é um csv válido do Discogs!')
+            if (count_erro_track > 0):
+                print('\n' + str(count_erro_track) + ' lançamento' + ('s' if count_erro_track > 1 else '') + ' com erro nas faixas.')
+                print(lista_com_erro)
+            else:
+                print('Nenhum lançamento com erro nas faixas encontrado.')
 
-args = parse_args()
+
+    except FileNotFoundError:
+        print("Arquivo não encontrado!")
 
 main()
